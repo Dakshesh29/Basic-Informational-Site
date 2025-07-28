04ddc452afe6a2f4de20270b20ce2942eeacea19
Dakshesh Bijawar <bdaksheshbijawar735@gmail.com>
2025-07-28 13:22:09 +0530

    complete server.js file

--- server.js
+++ server.js
@@ -7,4 +7,35 @@ const server = http.createServer((req, res) => {
   res.setHeader("Content-Type", "text/html");
 
   let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    case "/contact":
+      path += "contact.html";
+      res.statusCode = 200;
+      break;
+    default:
+      path += "404.html";
+      res.statusCode = 400;
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end("error");
+    } else {
+      res.end(data);
+    }
+  });
+});
+
+server.listen(3000, () => {
+  console.log("listening on port 3000");
 });
